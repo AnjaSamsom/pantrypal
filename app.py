408de4e62556b4dd9d73726783f32ed95a3f004f
AnjaSamsom <anjavt14@gmail.com>
2023-10-07 21:03:39 -0400

    not working for some reason

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
         submitted_pass = request.form['password']
         submitted_username = request.form['username']
 
-        if submitted_pass == "password" and submitted_username == "anja":
+        if submitted_pass == "samsom" and submitted_username == "anja":
             return render_template('home.html')
         else:
             return render_template("login.html")
@@ -23,11 +23,11 @@ def home():
 
 @app.route("/fridge", methods=['GET', 'POST'])
 def fridge():
-    data = fridge_list()
+    data = list("fridge.csv")
     return render_template('fridge.html', data = data)
 
-@app.route("/write_data.py", methods=["POST"])
-def write_data():
+@app.route("/write_data_use_up.py", methods=["POST"])
+def write_data_use_up():
     text = request.form.get("text")
 
     file = open("data.csv", "a")
@@ -36,6 +36,22 @@ def write_data():
     file.close()
 
     data = use_up()
-
-
     return render_template('fridge.html', data = data)
+
+
+""" @app.route("/shopping_list", methods=['GET', 'POST'])
+def shopping_list():
+    data = list("shopping_list.csv")
+    return render_template('shopping_list.html', data = data)
+
+@app.route("/write_data_add.py", methods=["POST"])
+def write_data_add():
+    text = request.form.get("text")
+
+    file = open("data.csv", "a")
+
+    file.write(text + ",")
+    file.close()
+
+    data = add()
+    return render_template('shopping_list.html', data = data) """
--- pantry_pal.py
+++ pantry_pal.py
@@ -1,9 +1,58 @@
 import csv
 
 def main():
-    print(use_up())
+    print("anja")
 
 
+def list(filename):
+    file = open(filename, "r")
+    data = list(csv.reader(file, delimiter=","))[0]
+    file.close()
+    return data
+
+
+def use_up():
+    file = open("fridge.csv", "r")
+    fridge = list(csv.reader(file, delimiter=","))[0]
+    file = open("data.csv", "r")
+    done = list(csv.reader(file, delimiter=","))[0]
+    result = []
+
+    for word in fridge:
+        if word in done:
+            print(word)
+        else:
+            result.append(word)
+
+    file = open("fridge.csv", "w")
+    for word in result:
+        file.write(word + ",")
+
+    file.close()
+
+    return result
+
+def add():
+    file = open("shopping_list.csv", "r")
+    shopping_list = list(csv.reader(file, delimiter=","))[0]
+    file = open("data.csv", "r")
+    data = list(csv.reader(file, delimiter=","))[0]
+    result = []
+
+    for word in data:
+        result.append(word)
+
+    file = open("shopping_list.csv", "a")
+    for word in result:
+        file.write(word + ",")
+
+    file.close()
+
+    return result
+
+
+
+""" 
 def load_pantry():
     file = open("pantry.csv", "r")
     data = list(csv.reader(file, delimiter=","))[0]
@@ -41,54 +90,6 @@ def compare(pantry, recipe):
 def add_to_list(ingredient):
     file = open("shopping_list.csv", "a")
     file.write(ingredient+",")
-
-def buy(ingredient):
-    file = open("pantry.csv", "a")
-    file.write(ingredient+",")
-
-
-    file = open("shopping_list.csv", "r")
-    data = list(csv.reader(file, delimiter=","))[0]
-
-    for item in data:
-        if item == ingredient:
-            item = ""
-
-    print(data)
-
-    file = open("shopping_list.csv", "w")
-    file.write("")
-    file = open("shopping_list.csv", "a")
-
-    for item in data:
-        file.write(item + ",")
-
-
-def fridge_list():
-    file = open("fridge.csv", "r")
-    data = list(csv.reader(file, delimiter=","))[0]
-    return data
-
-
-def use_up():
-    file = open("fridge.csv", "r")
-    fridge = list(csv.reader(file, delimiter=","))[0]
-    file = open("data.csv", "r")
-    done = list(csv.reader(file, delimiter=","))[0]
-    result = []
-
-    for word in fridge:
-        if word in done:
-            print(word)
-        else:
-            result.append(word)
-
-    file = open("fridge.csv", "w")
-    for word in result:
-        file.write(word + ",")
-
-    return result
-
-
+    file.close() """
 
 main()
